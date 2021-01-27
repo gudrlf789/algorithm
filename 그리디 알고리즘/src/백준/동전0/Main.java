8465084d9b0750abbe247f52f3f50787cded6fc2
Hyeong gil <gudrlf46@gmail.com>
2021-01-27 22:37:13 +0900

    백준 동전 0 문제 풀이 완료

--- 그리디 알고리즘/src/백준/동전0/Main.java
+++ 그리디 알고리즘/src/백준/동전0/Main.java
@@ -14,12 +14,30 @@ public class Main {
         }
 
         int[] coninValue = new int[countAndMoney[0]];
-        coninValue[0] = 1;
 
-        for(int i=1; i < coninValue.length ; i++){
+        for(int i=0; i < coninValue.length ; i++){
             coninValue[i] = scan.nextInt();
-
-
         }
+
+        int minusNum = 1;
+        int coinCount = 0;
+        while(true){
+            if(coninValue.length-minusNum < 0){
+                break;
+            }else {
+                int currentPrice = coninValue[coninValue.length - minusNum];
+                int divideValue = countAndMoney[1] / currentPrice;
+
+                if (divideValue == 0) {
+                    minusNum++;
+                } else {
+                    coinCount += divideValue;
+                    countAndMoney[1]  = countAndMoney[1] - (currentPrice * divideValue);
+                    minusNum++;
+                }
+            }
+        }
+
+        System.out.println(coinCount);
     }
 }
